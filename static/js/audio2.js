42c3eb7fe214e932459872a29f1e921bfc0a4fee
Irvin Hobba <skiphobba@gmail.com>
2020-08-09 14:18:21 -0400

    updated several sections of html - 3

---
+++ static/js/audio2.js
@@ -0,0 +1,71 @@
+// File Change/Selection section - controls canvas
+// I tried to create a function, cut and paste sections into the above, etc
+// but I couldn't get the buttons to work with the canvas
+
+window.onload = function() {
+  var file = document.getElementById("thefile");
+  var audio = document.getElementById("audio");
+  
+  file.onchange = function() {
+    var files = this.files;
+    audio.src = URL.createObjectURL(files[0]);
+    audio.load();
+    audio.play();
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth * .50;
+    canvas.height = window.innerHeight * .60;
+    var ctx = canvas.getContext("2d");
+
+    src.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.fftSize = 256;
+
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+
+      x = 0;
+
+      analyser.getByteFrequencyData(dataArray);
+
+      ctx.fillStyle = "#272822";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        
+        // var r = barHeight + (25 * (i/bufferLength));
+        // var g = 250 * (i/bufferLength);
+        // var b = 50;
+
+        var r = barHeight + (25 * (i/bufferLength));
+        var g = 25;
+        var b = 250 * (i/bufferLength);
+
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+        x += barWidth + 1;
+      }
+    }
+
+    audio.play();
+    renderFrame();
+  };
+};
